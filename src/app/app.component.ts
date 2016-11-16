8b0fac3e6e0a2645309bf7c1c2bc2c73ec16ee56
NicolasBouhours <nico53960@hotmail.fr>
2016-11-16 22:43:10 +0100

    STEP 20

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1> Inline template </h1>
+    <fa-other></fa-other>
+    <fa-another>
+      <div>
+        <h1>Hello!</h1>
+        <p>World!</p>
+      </div>
+    </fa-another>
+    <fa-another>
+      <p>Something else ! </p>
+    </fa-another>
+  `,
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
+})
+export class AppComponent {
+  title = 'I Changed it !';
+}
